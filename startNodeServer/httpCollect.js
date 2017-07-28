eb2d1136a74959f8c020bf0d12b1a3a8f494c6d7
Luqman Zagi <luqman.zagi@gmail.com>
2017-07-28 11:32:39 +0700

    HTTP Collect npm

--- startNodeServer/httpCollect.js
+++ startNodeServer/httpCollect.js
@@ -1,19 +1,33 @@
-// without npm
-
 var http = require('http');
+var bl = require('bl');
 var addr = process.argv[2];
 
-http.get(addr, function(res){
-    body = '';
-    res.setEncoding('utf8')
-    res.on('error', function(error){
-        console.log('error')
-    });
-    res.on('data',function(chunk){
-        body += chunk;
-    });
-    res.on('end',function(){
-        console.log(body.length);
-        console.log(body);
-    })
+// without npm
+
+// http.get(addr, function(res){
+//     body = '';
+//     res.setEncoding('utf8')
+//     res.on('error', function(error){
+//         console.log('error')
+//     });
+//     res.on('data',function(chunk){
+//         body += chunk;
+//     });
+//     res.on('end',function(){
+//         console.log(body.length);
+//         console.log(body);
+//     })
+// })
+
+//with npm
+
+http.get(addr,function(res){
+    res.setEncoding('utf8');
+    res.pipe(bl(function (err,data ){
+        if (err){
+            console.log(err);
+        }
+        console.log(data.length);
+        console.log(data.toString());
+    }))
 })
