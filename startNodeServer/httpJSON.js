8488762656259b2abf7d4238845d8850c2a9686e
Luqman Zagi <luqman.zagi@gmail.com>
2017-08-02 22:19:06 +0700

    JSON

---
+++ startNodeServer/httpJSON.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const url = require('url');
+
+var server = http.createServer(function (req, res){
+    res.writeHead(200,{'Content-type':'application/json'});
+    var urlQuerry = url.parse(req.url, true);
+    var timeParse = new Date(urlQuerry.query.iso);
+    if (urlQuerry.pathname == '/api/parsetime'&& urlQuerry.query.iso){
+        
+        var retTime = {
+            'hour' : timeParse.getHours(),
+            'minute': timeParse.getMinutes(),
+            'second': timeParse.getSeconds()
+        };
+        res.end(JSON.stringify(retTime));
+    }
+    else if (urlQuerry.pathname == '/api/unixtime'){
+        res.end(JSON.stringify({ 'unixtime': timeParse.getTime()}))
+    }
+    else{
+        console.log('xxx')
+    }
+}).listen(process.argv[2]);
